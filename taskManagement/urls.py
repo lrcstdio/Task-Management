bf95d82f68b6ee680fb3391c4602e908dd4c90c8
liuren <316294622@qq.com>
2018-06-05 19:52:52 +0800

    add some function

--- taskManagement/urls.py
+++ taskManagement/urls.py
@@ -30,4 +30,30 @@ urlpatterns = [
     url(r'^query/$',views.query,name='query'),
     url(r'^delete/(?P<pk>\d+)/$', views.delete, name='delete'),
     url(r'^revise/(?P<pk>\d+)/$', views.revise, name='revise'),
+
+    url(r'^reset/$',auth_views.PasswordResetView.as_view(
+            template_name='password_reset.html',
+            email_template_name='password_reset_email.html',
+            subject_template_name='password_reset_subject.txt'
+            ),name='password_reset'),
+
+    url(r'^reset/done/$',auth_views.PasswordResetDoneView.as_view(
+    template_name='password_reset_done.html'),name='password_reset_done'),
+
+    url(r'^reset/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
+        auth_views.PasswordResetConfirmView.as_view(template_name='password_reset_confirm.html'),
+        name='password_reset_confirm'),
+
+    url(r'^reset/complete/$',
+        auth_views.PasswordResetCompleteView.as_view(
+        template_name='password_reset_complete.html'),
+        name='password_reset_complete'),
+
+    url(r'^settings/password/$', auth_views.PasswordChangeView.as_view(
+        template_name='password_change.html'),
+        name='password_change'),
+
+    url(r'^settings/password/done/$', auth_views.PasswordChangeDoneView.as_view(
+        template_name='password_change_done.html'),
+        name='password_change_done'),
 ]
